1025e993350891f1ce23fdc9c5b10ec0d14673a6
SachinGunjal001 <sachingunjal26x@gmail.com>
2025-11-11 10:40:48 +0530

    Data Structures changed

--- InsuraceData_SQL.sql
+++ InsuraceData_SQL.sql
@@ -1,2 +1,13 @@
-CREATE DATABASE InsurancePremium
+CREATE DATABASE InsurancePremiumData
 
+
+Use InsurancePremiumData
+
+
+select * from [dbo].[FCT_Insurance_Policy_Table]
+select * from [dbo].[DM_Customer_Detail_Table]
+select * from [dbo].[DM_Policy_Type]
+select * from [dbo].[DM_Regional_Manager]
+select * from [dbo].[DM_Zonal_Manager]
+select * from [dbo].[DM_Policy_Protection_Plan]
+select * from [dbo].[DM_Insurance_Agent_Table]
